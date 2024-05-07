74ff6d33e49e6295a1398d3ab49b5b03f225c013
AdelMysterio <storliniadelmo@gmail.com>
2024-05-07 13:13:46 +0200

    Exercise Done

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const nome = prompt ("Type your name")
+const surname = prompt ("Type your surname")
+const color = prompt ("Type your favorite color")
+
+document.getElementById("pwgen").innerHTML = (nome + surname + color + '24')
